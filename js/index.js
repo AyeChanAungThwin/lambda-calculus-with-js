938b3969ddeea002fc5ae06c54618cada91979ff
demodemo834 <ayechanaungthwin@gmail.com>
2022-04-07 11:30:55 +0630

    created object

--- js/index.js
+++ js/index.js
@@ -20,85 +20,93 @@ function getHtmlTag(htmlTag) {
 }
 
 function startAnimation() {
+    let idiot = new Bird('idiot');
     setInterval(function() {
-        bird('idiot')
+        idiot.getBird()
+    }, 20);
+    
+    let kestrel = new Bird('kestrel-a');
+    setInterval(function() {
+        kestrel.getBird()
     }, 20);
 }
 
-let areInitialized = true;
-let startDelayAnimation = 0;
-let stopDelayAnimation = 0;
-let initL2R = 0;
-let initT2B = 0;
-let L2R = initL2R;
-let T2B = initT2B;
-let pauseL2R = false;
-let pauseT2B = true;
+function Bird(birdName) {
+    this.areInitialized = true;
+    this.startDelayAnimation = 0;
+    this.stopDelayAnimation = 0;
+    this.initL2R = 0;
+    this.initT2B = 0;
+    this.L2R = this.initL2R;
+    this.T2B = this.initL2R;
+    this.pauseL2R = false;
+    this.pauseT2B = true;
 
-function resetBird() {
-    startDelayAnimation = 0;
-    stopDelayAnimation = 0;
-    L2R = initL2R;
-    T2B = initT2B;
-    pauseL2R = false;
-    pauseT2B = true;
-}
+    this.resetBird = function() {
+        this.startDelayAnimation = 0;
+        this.stopDelayAnimation = 0;
+        this.L2R = this.initL2R;
+        this.T2B = this.initT2B;
+        this.pauseL2R = false;
+        this.pauseT2B = true;
+    };
 
-function bird(inputName) {
-    //get html tags
-    let bird = getHtmlTag(`.${inputName}`);
-    let ear = getPropertyValue(bird, "--bird-ear");
-    let throat = getPropertyValue(bird, "--bird-throat");
-    let pipe = getHtmlTag(`.${inputName} .pipe`);
-    let input = getHtmlTag(`.${inputName}-input`);
+    this.getBird = function() {
+        //get html tags
+        let bird = getHtmlTag(`.${birdName}`);
+        let ear = getPropertyValue(bird, "--bird-ear");
+        let throat = getPropertyValue(bird, "--bird-throat");
+        let pipe = getHtmlTag(`.${birdName} .pipe`);
+        let input = getHtmlTag(`.${birdName}-input`);
 
-    //use as const
-    if (areInitialized) {
-        initL2R = +(getPropertyValue(input, '--from-left'));
-        initT2B = +(getPropertyValue(input, '--from-top'));
-        L2R = initL2R;
-        T2B = initT2B;
-        areInitialized = false;
-    }
+        //use as const
+        if (this.areInitialized) {
+            this.initL2R = +(getPropertyValue(input, '--from-left'));
+            this.initT2B = +(getPropertyValue(input, '--from-top'));
+            this.L2R = this.initL2R;
+            this.T2B = this.initT2B;
+            this.areInitialized = false;
+        }
 
-    //lengths
-    let leftPipeLength = +(getPropertyValue(pipe, "--left-pipe-length"));
-    let midPoint = leftPipeLength;
-    let fromLeft = 0;
-    fromLeft = initL2R;
-    if (initL2R<0) fromLeft = initL2R*-1;
-    let endPoint = 100+fromLeft;
-    let differenceBetweenThroat2Ear = throat-ear;
+        //lengths
+        let leftPipeLength = +(getPropertyValue(pipe, "--left-pipe-length"));
+        let midPoint = leftPipeLength;
+        let fromLeft = 0;
+        fromLeft = this.initL2R;
+        if (this.initL2R < 0) fromLeft = this.initL2R * -1;
+        let endPoint = 100 + fromLeft;
+        let differenceBetweenThroat2Ear = throat - ear;
 
-    //set values
-    setPropertyValue(input, '--from-left', L2R);
-    setPropertyValue(input, '--from-top', T2B);
-    
-    //start animation
-    startDelayAnimation++;
-    if (startDelayAnimation>=20&&!pauseL2R) L2R+=2;
-    if (!pauseT2B) T2B+=2;
+        //set values
+        setPropertyValue(input, '--from-left', this.L2R);
+        setPropertyValue(input, '--from-top', this.T2B);
 
-    if (L2R>=midPoint) {
-        pauseL2R = !!differenceBetweenThroat2Ear;
-        pauseT2B = !pauseL2R;
-    }
+        //start animation
+        this.startDelayAnimation++;
+        if (this.startDelayAnimation >= 20 && !this.pauseL2R) this.L2R += 2;
+        if (!this.pauseT2B) this.T2B += 2;
 
-    let topToBottomLength = differenceBetweenThroat2Ear+initT2B;
-    if (T2B>=topToBottomLength) {
-        pauseT2B = true;
-        pauseL2R = !pauseT2B;
-    }
+        if (this.L2R >= midPoint) {
+            this.pauseL2R = !!differenceBetweenThroat2Ear;
+            this.pauseT2B = !this.pauseL2R;
+        }
 
-    if (L2R>=endPoint) {
-        hide(pipe);
-        pauseL2R = true;
-        stopDelayAnimation++;
-    }
-    if (stopDelayAnimation>=20) {
-        resetBird();
-        unhide(pipe);
-    }
+        let topToBottomLength = differenceBetweenThroat2Ear + this.initT2B;
+        if (this.T2B >= topToBottomLength) {
+            this.pauseT2B = true;
+            this.pauseL2R = !this.pauseT2B;
+        }
+
+        if (this.L2R >= endPoint) {
+            hide(pipe);
+            this.pauseL2R = true;
+            this.stopDelayAnimation++;
+        }
+        if (this.stopDelayAnimation >= 20) {
+            this.resetBird();
+            unhide(pipe);
+        }
+    };
 }
 
 startAnimation();
